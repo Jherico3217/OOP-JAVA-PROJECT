915f1e851fa92ac8faa374b7a5cdabf0ca4d5c55
Jherico3217 <142374799+Jherico3217@users.noreply.github.com>
2023-12-07 23:04:56 +0800

    New push for main.java
    Version 1.1

--- Final Project/src/Main.java
+++ Final Project/src/Main.java
@@ -1,50 +1,73 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Are you an admin or a user? (Enter 'admin' or 'user'): ");
-        String userType = scanner.nextLine();
+        CashierSystem cashierSystem = new CashierSystem();
 
-        if (userType.equalsIgnoreCase("admin")) {
-            Admin admin = new Admin();
-            System.out.print("Enter admin username: ");
-            String adminUsername = scanner.nextLine();
-            System.out.print("Enter admin password: ");
-            String adminPassword = scanner.nextLine();
+        while (true) {
+            System.out.println("Main Menu: ");
+            System.out.println("1. Admin");
+            System.out.println("2. Client");
+            System.out.println("3. Exit");
+            int choice = scanner.nextInt();
 
-            if (admin.login(adminUsername, adminPassword)) {
-                System.out.print("Admin login successful!");
-
-                // Perform admin-specific operations
-
-            } else {
-                System.out.print("Invalid admin credentials. Login failed.");
+            switch (choice) {
+                case 1:
+                    adminInterface(scanner, cashierSystem);
+                    break;
+                case 2:
+                    clientInterface(scanner, cashierSystem);
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please enter 1, 2, or 3.");
             }
-        } else if (userType.equalsIgnoreCase("user")) {
-            User user = new User();
-            System.out.print("Enter user username: ");
-            String userUsername = scanner.nextLine();
-            System.out.print("Enter user password: ");
-            String userPassword = scanner.nextLine();
-
-            if (user.login(userUsername, userPassword)) {
-                System.out.println("User login successful!");
-
-                Student student = new Student("Alice Mendoza", 2);
-                Receipt receipt = new Receipt(student);
-                receipt.addTransaction(new Transaction ("T001", 75.50));
-                receipt.addTransaction(new Transaction ("T002", 60.25));
-                receipt.addTransaction(new Transaction ("T003", 50.75));
-                receipt.generateReceipt();
-
-            } else {
-                System.out.println("Invalid user credentials. Login failed.");
-            }
-        } else {
-            System.out.println("Invalid input. Please enter 'admin' or 'user'.");
         }
-        scanner.close();
+    }
+
+    private static void adminInterface(Scanner scanner, CashierSystem cashierSystem) {
+        while (true) {
+            System.out.println("Admin Interface:");
+            System.out.println("1. Add Transaction");
+            System.out.println("2. Remove Transaction");
+            System.out.println("3. View Transaction History");
+            System.out.println("4. Back to Main Menu");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter client name: ");
+                    String clientName = scanner.nextLine();
+                    System.out.println("Enter product: ");
+                    String product = scanner.nextLine();
+                    System.out.println("Enter price: ");
+                    double price = scanner.nextDouble();
+                    cashierSystem.addTransaction(clientName, product, price);
+                    break;
+                case 2:
+                    System.out.println("Enter index of transaction to remove: ");
+                    int index = scanner.nextInt();
+                    cashierSystem.removeTransaction(index);
+                    break;
+                case 3:
+                    cashierSystem.viewTransactionHistory();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+            }
+        }
+    }
+
+    private static void clientInterface(Scanner scanner, CashierSystem cashierSystem) {
+        System.out.println("Enter client name: ");
+        String clientName = scanner.nextLine();
+        cashierSystem.viewTransactions(clientName);
     }
 }
